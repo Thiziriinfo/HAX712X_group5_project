6d08340641e7a6baa28107ef85e46afdeea10199
Guillaume-BR <guillaume.bernardreymond@gmail.com>
2023-11-21 17:20:17 +0100

    date début et fin

--- traitement_week.py
+++ traitement_week.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import pandas as pd
 import numpy as np
 from datetime import datetime
@@ -13,20 +13,24 @@ df = pd.read_csv("./data/mesure_horaire_view.csv")
 # nettoyage df
 df = df.drop(["date_fin", "statut_valid", "x_l93", "y_l93", "geom", "metrique"], axis=1)
 
+
 # %%
-# liste des villes et des polluants
+# liste des villes et des polluants triés
 villes = df["nom_com"].unique().tolist()
 villes.sort()
 polluants = df["nom_polluant"].unique().tolist()
 polluants.sort()
 
+
 # %%
 # fonction qui fait la sélection ville et polluant
 def selection(ville, polluant):
+    if ville == "MONTPELLIER":
+        df["nom_station"] = df["nom_station"].replace(
+            ["Montpelier Pere Louis Trafic"], "Montpelier Antigone Trafic"
+        )
     df_1 = df.loc[(df["nom_com"] == ville) & (df["nom_polluant"] == polluant), :]
     return df_1
-
-
 
 
 # %%
@@ -35,25 +39,29 @@ def graphique(ville, polluant):
     df_pv = selection(ville, polluant)
     stations = df_pv["nom_station"].unique()
     nb_stations = len(stations)
-    fig, axes = plt.subplots(nb_stations, 1, figsize=(10, 15) , sharex=True)
-    fig.suptitle('Pollution à Montpellier', fontsize=16)
+    fig, axes = plt.subplots(nb_stations, 1, figsize=(10, 15), sharex=True)
+    fig.suptitle("Pollution selon le jour de la semaine à Montpellier", fontsize=16)
 
     for i in range(nb_stations):
         df_pvs = df_pv.loc[df_pv["nom_station"] == stations[i]]
-        df_pvs['date_debut'] = df_pvs['date_debut'].apply(lambda _:datetime.strptime(_,"%Y-%m-%d %H:%M:%S"))
-        df_pvs = df_pvs.set_index(['date_debut'])
-        df_pvs['weekday'] = df_pvs.index.weekday
-        pollution_week = df_pvs.groupby(['weekday', df_pvs.index.hour])[
-    'valeur'].mean().unstack(level=0)
+        df_pvs["date_debut"] = df_pvs["date_debut"].apply(
+            lambda _: datetime.strptime(_, "%Y-%m-%d %H:%M:%S")
+        )
+        df_pvs = df_pvs.set_index(["date_debut"])
+        df_pvs["weekday"] = df_pvs.index.weekday
+        pollution_week = (
+            df_pvs.groupby(["weekday", df_pvs.index.hour])["valeur"]
+            .mean()
+            .unstack(level=0)
+        )
         axes[i].plot(pollution_week)
-        axes[i].set_xlabel("Date")
+        axes[i].set_xticklabels(np.arange(0, 24), rotation=45)
         axes[i].set_ylabel("Concentration en µg/m3")
         axes[i].set_title(
-            "Concentration du "
-            + str(polluant)
-            + " à "
-            + str(stations[i])
+            "Concentration du " + str(polluant) + " à " + str(stations[i])
         )
+        axes[i].legend(
+        ).set_visible(True)
         axes[i].grid(True)
 
     plt.show()
--- traitement_year.py
+++ traitement_year.py
@@ -11,7 +11,7 @@ df = pd.read_csv("./data/mesure_horaire_view.csv")
 
 # %%
 # nettoyage df
-df = df.drop(["date_fin", "statut_valid", "x_l93", "y_l93", "geom", "metrique"], axis=1)
+df = df.drop(["date_fin", "statut_valid", "x_l93", "y_l93", "geom", "metrique", "id", ], axis=1)
 
 # %%
 # liste des villes et des polluants
@@ -24,32 +24,22 @@ polluants.sort()
 # %%
 # fonction qui fait la sélection ville et polluant
 def selection(ville, polluant):
+    if ville == 'MONTPELLIER':
+        df["nom_station"] = df["nom_station"].replace(['Montpelier Pere Louis Trafic'], 'Montpelier Antigone Trafic')
     df_1 = df.loc[(df["nom_com"] == ville) & (df["nom_polluant"] == polluant), :]
     return df_1
 
 
-# %%
-test = selection(villes[3], polluants[0])
-test2 = selection("MONTPELLIER", "NOX")
-
-
-# fonction sur qui crée les listes pour le graphique
-def liste(df, nb_heure):
-    dates = df["date_debut"][1:nb_heure].to_list()
-    formatting = "%Y-%m-%d %H:%M:%S"
-    x = [datetime.strptime(date, formatting) for date in dates]
-    y = df["valeur"][1:nb_heure].to_list()
-    return x, y
-
 
 # %%
 # Fonction qui trace le graphique
-def graphique(ville, polluant):
+def graphique(ville, polluant, debut, fin):
     df_pv = selection(ville, polluant)
     nom_stations = df_pv["nom_station"].unique()
     nb_stations = len(nom_stations)
     fig, axes = plt.subplots(nb_stations, 1, figsize=(10, 15), sharex=True)
-    fig.suptitle("Pollution au " + str(polluant) + " à " + str(ville), fontsize=16)
+    fig.suptitle("Pollution au " + str(polluant) +
+                 " à " + str(ville) + " du " + str(debut) + " à " + str(fin), fontsize=16)
 
     for i in range(nb_stations):
         df_pvs = df_pv.loc[df_pv["nom_station"] == nom_stations[i]]
@@ -57,14 +47,19 @@ def graphique(ville, polluant):
             lambda _: datetime.strptime(_, "%Y-%m-%d %H:%M:%S")
         )
         df_pvs = df_pvs.set_index(["date_debut"])
+        df_pvs = df_pvs.loc[debut:fin]
         axes[i].plot(df_pvs["valeur"].resample("d").mean())
+        for label in axes[i].get_xticklabels():
+            label.set_ha("right")
+            label.set_rotation(45)
         axes[i].set_ylabel("Concentration en µg/m3")
         axes[i].set_title(
-            "Concentration du " + str(polluant) + " à " + str(nom_stations[i])
+            "Concentration du " + str(polluant) + " au " + str(nom_stations[i])
         )
         axes[i].grid(True)
 
     plt.show()
+
 
 
 # %%
