2f5a9954f48dfec424aae41fe7fe1165792bb4bb
Guillaume-BR <guillaume.bernardreymond@gmail.com>
2023-12-04 15:04:58 +0100

    traitement.py

--- code_a_implementer/traitement_year.py
+++ code_a_implementer/traitement_year.py
@@ -3,67 +3,62 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 
 # %%
 # lecture du dataframe
-df = pd.read_csv("./data/mesure_horaire_view.csv")
+df_atmo = pd.read_csv("/home/guillaume/project_2023_2024/HAX712X_group5_project/data/donnees_atmo.csv")
 
-# %%
-# nettoyage df
-df = df.drop(["date_fin", "statut_valid", "x_l93", "y_l93", "geom", "metrique"], axis=1)
-
-# %%
-# liste des villes et des polluants
-villes = df["nom_com"].unique().tolist()
-villes.sort()
-polluants = df["nom_polluant"].unique().tolist()
-polluants.sort()
-
-
-# %%
-# fonction qui fait la sélection ville et polluant
+#%%
 def selection(ville, polluant):
-    df_1 = df.loc[(df["nom_com"] == ville) & (df["nom_polluant"] == polluant), :]
+    if ville == "MONTPELLIER":
+        df_atmo["nom_station"] = df_atmo["nom_station"].replace(
+            ["Montpelier Pere Louis Trafic"], "Montpelier Antigone Trafic"
+        )
+    df_1 = df_atmo.loc[
+        (df_atmo["nom_com"] == ville) & (df_atmo["nom_polluant"] == polluant), :
+    ]
     return df_1
-
-
-# %%
-test = selection(villes[3], polluants[0])
-test2 = selection("MONTPELLIER", "NOX")
-
-
-# fonction sur qui crée les listes pour le graphique
-def liste(df, nb_heure):
-    dates = df["date_debut"][1:nb_heure].to_list()
-    formatting = "%Y-%m-%d %H:%M:%S"
-    x = [datetime.strptime(date, formatting) for date in dates]
-    y = df["valeur"][1:nb_heure].to_list()
-    return x, y
 
 
 # %%
 # Fonction qui trace le graphique
 def graphique(ville, polluant):
+    # sélection : pas utile
     df_pv = selection(ville, polluant)
+    # les différentes stations
     nom_stations = df_pv["nom_station"].unique()
     nb_stations = len(nom_stations)
-    fig, axes = plt.subplots(nb_stations, 1, figsize=(10, 15), sharex=True)
+    # plusieurs graphiques
+    if nb_stations == 1:
+        fig, axes = plt.subplots(1, 1, figsize=(10, 5), layout="constrained")  # Créer une seule sous-figure
+        axes = [axes]  # Mettre l'unique axe dans une liste
+    else:
+        fig, axes = plt.subplots(nb_stations, 1, figsize=(10, 15), sharex=True, layout="constrained")
+    # titre général
     fig.suptitle("Pollution au " + str(polluant) + " à " + str(ville), fontsize=16)
 
     for i in range(nb_stations):
+        # on garde seulement les données de la station i
         df_pvs = df_pv.loc[df_pv["nom_station"] == nom_stations[i]]
+        # transformation en datetime de date_debut
         df_pvs["date_debut"] = df_pvs["date_debut"].apply(
             lambda _: datetime.strptime(_, "%Y-%m-%d %H:%M:%S")
         )
+        # datetime devient index
         df_pvs = df_pvs.set_index(["date_debut"])
+        # on moyennise par jour
         axes[i].plot(df_pvs["valeur"].resample("d").mean())
+        # labellisations et titre
+        for label in axes[i].get_xticklabels():
+            label.set_ha("right")
+            label.set_rotation(45)
         axes[i].set_ylabel("Concentration en µg/m3")
         axes[i].set_title(
             "Concentration du " + str(polluant) + " à " + str(nom_stations[i])
         )
         axes[i].grid(True)
-
+        
+    plt.savefig("year.svg", dpi=300, transparent=True)
     plt.show()
 
 
